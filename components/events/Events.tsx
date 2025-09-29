f7134354eeeb385f8a630de8fe78db1f1e33c97a
Kourai9 <luigikarlblimos@gmail.com>
2025-09-30 07:10:20 +0800

    feat: Event Cards initial push. Cleared for ESLint Erros.

--- components/events/Events.tsx
+++ components/events/Events.tsx
@@ -3,7 +3,9 @@ import EventCards from "./components/EventCards";
 
 export default function Events() {
   return (
-    <div className="min-h-screen bg-white flex items-center justify-center">
+    <div className="min-h-screen bg-black flex items-center justify-around">
+      <EventCards />
+      <EventCards />
       <EventCards />
     </div>
   );
--- components/events/components/EventCards.tsx
+++ components/events/components/EventCards.tsx
@@ -1,38 +1,55 @@
 import React from "react";
 import Link from "next/link";
 
-// interface Event {
-//   eventId: string;
-//   name: string;
-//   description: string;
-//   venue: string;
-//   startDate: string;
-//   dayOfWeek: string;
-//   Price: string;
-//   endDate: string;
-//   createdAt: string;
-//   updatedAt: string;
-// }
+interface Event {
+  eventId: string;
+  name: string;
+  semester: string;
+  venue: string;
+  startDate: string;
+  dayOfWeek: string;
+  price: string;
+  endDate: string;
+  registrant: number;
+}
+
+const event: Event = {
+  eventId: "sampleID",
+  name: "EVENT TITLE",
+  semester: "1st",
+  venue: "FEU Tech Student's Plaza",
+  startDate: "September 30 | 7:00PM",
+  dayOfWeek: "Wed",
+  price: "Free",
+  endDate: "September 30 | 9:00PM",
+  registrant: 30,
+};
 
 export default function EventCards() {
   return (
     <Link href="/events/title" passHref>
-      <div className="w-[537px] h-[657px] flex flex-col justify-between cursor-pointer hover:opacity-90 transition">
-        <div className="w-full h-[45%] flex items-center justify-center bg-emerald-700 rounded-lg">
-          This is where the image would be
+      <div className="w-[409px] h-[578px] bg-[white] relative text-black overflow-hidden">
+        <div className="headers font-['Montserrat'] text-[12px] absolute rotate-270 text-right w-[123px] origin-top-left h-[36px] left-[30px] top-[153px]">
+          {/* What Semester */}
+          <p className="m-0">{event.semester} Semester</p>
+          <p className="m-0 -mt-1 font-bold">ACM Event Series</p>
         </div>
-        <div className="w-full h-[55%] flex flex-col justify-between text-black">
-          <h1 className="font-['Fjalla-One'] text-[64px] "> Event Title</h1>
-          <p className="font-['Fjalla-One'] text-[32px]">Hosted by: ACM</p>
-          <div className="w-full flex justify-between align-left">
-            <p>Weekday</p>
-            <p>Date</p>
-            <p>Time</p>
-          </div>
-          <div className="w-full flex justify-between align-left">
-            <p>30 people going</p>
-            <p>30 pesos</p>
-          </div>
+        <div className="left-[29px] right-[29px] bottom-[141px] Title font-['Roller-Coaster'] absolute">
+          {/* ACM + Event Title */}
+          <p className="text-[43px] h-[33px]">ACM</p>
+          <h1 className="text-[86px] h-[66px]">{event.name}</h1>
+        </div>
+        <div className="eventDetails absolute left-[29px] right-[29px] bottom-[75px] font-['Trochut'] text-[16px] gap-0">
+          {/* Venue + Schedule */}
+          <p className="m-0">Where: {event.venue}</p>
+          <p className="-mt-1">
+            {event.dayOfWeek}, {event.startDate}
+          </p>
+        </div>
+        <div className="absolute bottom-[30px] right-[29px] left-[29px] footer font-['Montserrat'] text-[10px] flex justify-between">
+          {/* Number of People Going + Price of Event */}
+          <p>{event.registrant} People Going</p>
+          <p>{event.price}</p>
         </div>
       </div>
     </Link>
